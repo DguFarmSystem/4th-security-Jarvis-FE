299668976bfd4802f1f8104472925d3e903681e8
MINJU <mjryu00211@gmail.com>
2025-07-23 15:27:49 +0900

    [feat] #1 Implemented Icons

---
+++ src/assets/icons/DashboardIcon.tsx
@@ -0,0 +1,7 @@
+export const DashboardIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 35 35" fill="none">
+  <path d="M18.9583 13.125V4.375H30.625V13.125H18.9583ZM4.375 18.9583V4.375H16.0417V18.9583H4.375ZM18.9583 30.625V16.0417H30.625V30.625H18.9583ZM4.375 30.625V21.875H16.0417V30.625H4.375Z" fill="white"/>
+</svg>
+    );
+}
