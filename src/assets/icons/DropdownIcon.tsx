ed30a19b3ade502b02e4ee805b4db230d49d3249
MINJU <mjryu00211@gmail.com>
2025-07-27 01:13:30 +0900

    [chore] Built Design System

---
+++ src/assets/icons/DropdownIcon.tsx
@@ -0,0 +1,7 @@
+export const DropdownIcon = () => {
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="12" viewBox="0 0 24 12" fill="none">
+  <g opacity="0.7">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M11.2894 10.1569L5.63235 4.49994L7.04635 3.08594L11.9964 8.03594L16.9464 3.08594L18.3604 4.49994L12.7034 10.1569C12.5158 10.3444 12.2615 10.4497 11.9964 10.4497C11.7312 10.4497 11.4769 10.3444 11.2894 10.1569Z" fill="black"/>
+  </g>
+</svg>
+}
