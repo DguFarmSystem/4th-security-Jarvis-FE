9556772b8f73fd6eb45426619f532950c4121799
minij02 <mjryu00211@gmail.com>
2025-08-18 19:05:18 +0900

    [fix] disabled MSW

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { worker } from './mocks/browser.ts'
+// import { worker } from './mocks/browser.ts'
 import "../src/styles/theme.css";
 
-worker.start();
+// worker.start();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
